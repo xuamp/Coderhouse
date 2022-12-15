92cdde3196c9fd8d1f4724b5e53bd053f0aa7213
xuamp <108774140+xuamp@users.noreply.github.com>
2022-12-15 15:43:00 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import CartWidget from './components/CartWidget/CartWidget';
+import NavBar from './components/NavBar/NavBar';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
