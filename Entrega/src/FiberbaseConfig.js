f02b119de5c8526adebffe52908aa5dec76d4b3f
xuamp <108774140+xuamp@users.noreply.github.com>
2023-01-23 23:34:14 -0300

    Add files via upload

---
+++ Entrega/src/FiberbaseConfig.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCE_0LoNRTmHJszgC1gP1iMKNkoptfFwG4",
+  authDomain: "papelera-ecommerce.firebaseapp.com",
+  projectId: "papelera-ecommerce",
+  storageBucket: "papelera-ecommerce.appspot.com",
+  messagingSenderId: "199855853674",
+  appId: "1:199855853674:web:25d26ec24afc74344afa13",
+};
+
+export const app = initializeApp(firebaseConfig);
